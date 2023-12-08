417c83ac28e3debde7f8876df9c77d7e7068b70c
starrything <starrything@gmail.com>
2023-12-08 11:07:20 +0900

    iljin contents 작성 중

--- src/_contents/2022-02-iljin.ts
+++ src/_contents/2022-02-iljin.ts
@@ -3,28 +3,66 @@ export const getContents = {
 
   post: {
     title: 'iljin',
+    id: 'e99f09a7-dd88-49d5-b1c8-1daf80c2d7b1',
 
     author: {
       avatarUrl: 'https://api-dev-minimal-v510.vercel.app/assets/images/avatar/avatar_1.jpg',
       name: 'Jonghyun',
     },
 
-    content:
-      '\n\n<h1>Heading H1</h1><br/><br/>\n\n<h2>Heading H2</h2><br/><br/>\n\n<h3>Heading H3</h3><br/><br/>\n\n<h4>Heading H4</h4><br/><br/>\n\n<h5>Heading H5</h5><br/><br/>\n\n<h6>Heading H6</h6><br/><br/>\n\n<br/><br/><hr><br/><br/>\n\n<h3>Paragraph</h3><br/>\n\n\n<p>What is MTAweb Directory?</p><br/>\n\n<p>So you have heard about this site or you have been to it, but you cannot figure out what it is or what it can do. MTA web directory is the simplest way in which one can bid on a link, or a few links if they wish to do so. The link directory on MTA displays all of the links it currently has, and does so in alphabetical order, which makes it much easier for someone to find what they are looking for if it is something specific and they do not want to go through all the other sites and links as well. It allows you to start your bid at the bottom and slowly work your way to the top of the list.</p><br/>\n\n<p>With a very low costing starting bid of just $1, you are guaranteed to have a spot in MTA’s successful directory list. When you would like to increase your bid to one of the top positions, you have to know that this would be a wise decision to make as it will not only get your link to be at a higher point in the directory but it will also give you a chance to have your site advertised with the rest of the top ten on the home page of the website. This means that when visitors come to MTAweb.com, your site will be one of the first things they see. In other words, you stand a great chance at getting a comeback to your site sooner than you thought.</p><br/>\n\n<p><strong>This is strong text.</strong></p><br/>\n\n<p><em>This is italic text</em></p><br/>\n\n<p><u>This is underline text</u></p>\n\n<br/><br/><hr><br/><br/>\n\n<h3>Unordered list</h3><br/>\n\n<ul>\n    <li>Implements\n        <a href="https://docs-minimals.vercel.app/introduction">This is an external link</a>\n    </li>\n    <li>Implements\n        <a href="/dashboard/blog">This is an inside link</a>\n    </li>\n    <li>Renders actual, "native" React DOM elements</li>\n    <li>Allows you to escape or skip HTML (try toggling the checkboxes above)</li>\n    <li>If you escape or skip the HTML, no dangerouslySetInnerHTML is used! Yay!</li>\n</ul>\n\n<br/><br/><hr><br/><br/>\n\n<h3>Ordered list</h3>\n\n<br/>\n<ol>\n    <li>Analysis</li>\n    <li>Design</li>\n    <li>Implementation</li>\n</ol>\n\n<br/><br/><hr><br/><br/>\n\n<h3>Blockquote</h3>\n<br/>\n\n<blockquote>Life is short, Smile while you still have teeth!&nbsp;</blockquote>\n\n<br/><br/><hr><br/><br/>\n\n<h3>Block Code</h3>\n\n<br/>\n\n<pre class="ql-syntax" spellcheck="false"><span class="hljs-keyword">import</span> <span class="hljs-title class_">React</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react\'</span>;\n<span class="hljs-keyword">import</span> <span class="hljs-title class_">ReactDOM</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react-dom\'</span>;\n<span class="hljs-keyword">import</span> <span class="hljs-title class_">ReactMarkdown</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react-markdown\'</span>;\n<span class="hljs-keyword">import</span> rehypeHighlight <span class="hljs-keyword">from</span> <span class="hljs-string">\'rehype-highlight\'</span>;\n\n<span class="hljs-title class_">ReactDOM</span>.<span class="hljs-title function_">render</span>(\n  <span class="hljs-tag">&lt;<span class="hljs-name">ReactMarkdown</span> <span class="hljs-attr">rehypePlugins</span>=<span class="hljs-string">{[rehypeHighlight]}</span>&gt;</span>{\'# Your markdown here\'}<span class="hljs-tag">&lt;/<span class="hljs-name">ReactMarkdown</span>&gt;</span>,\n  <span class="hljs-variable language_">document</span>.<span class="hljs-title function_">querySelector</span>(<span class="hljs-string">\'#content\'</span>)\n);\n</pre>\n\n<br/>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<br/>\n<br/>\n<p>Why do we use it?</p>\n<br/>\n<br/>\n\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\n\n<br/>\n<br/>\n<p>\n<img src=https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_5.jpg />\n</p>\n<br/>\n<br/>\n\n<p>\n    It is important that you buy links because the links are what get you the results that you want. The popularity of the links that are listed in the MTA directory is in fact one of the most important factors in the performance of the search engine. Links are important and this is why you have to purchase a link in order to bid on something and the best part is that a link will only cost you $1, which is nothing compared to what you would pay if you decided to do it through any other company or website.\n</p>\n\n<br/>\n<br/>\n<p>\n    <img src=https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_14.jpg />\n</p>\n<br/>\n<br/>\n',
-    coverUrl: 'https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_1.jpg',
     createdAt: '2022-02-14T08:32:59.689Z',
+    coverUrl: 'https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_1.jpg',
 
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-    id: 'e99f09a7-dd88-49d5-b1c8-1daf80c2d7b1',
+    metaDescription: 'WEB Developer, ILJIN C&S',
+    metaKeywords: ['web', 'db', 'dev', 'oss', 'iljin'],
+    metaTitle: 'Career at ILJIN',
 
-    metaDescription: 'The starting point for your next project with Minimal UI Kit',
-    metaKeywords: ['Fitness', 'Nature', 'Business'],
-    metaTitle: 'Minimal UI Kit',
-
-    pageTitle: 'ILJIN C&S (2014.12.-2022.02.)',
     publish: 'draft',
-    tags: ['Technology', 'Marketing', 'Design', 'Photography', 'Art'],
+    pageTitle: 'ILJIN C&S (2014.12.-2022.02.)',
+    description: '2014년 12월 입사해 2022년 2월까지 만 7년 이상 근무한 ILJIN C&S에서의 경력입니다.',
+
+    content: `<hr>
+      <h1>작성 중입니다. (2023.12.08.)</h1><br/>
+      <br/><br/>
+      <h3>요약</h3><br/>
+      <p>어떻게 IT개발 직무를 시작하였나?</p><br/>
+      <p>2014년 12월 일진 씨앤에스 ERP파트로 입사해서 ERP와 인터페이스하는 웹 시스템 담당자로 업무를 시작하였다.</p>
+      <p><em>일진 씨앤에스는 '일진'그룹의 IT계열사이다. 그로인해 대내사업인 ITO매출이 거의 대부분을 이루었다.</em></p><br/>
+      <p>먼저, <b>웹 시스템 운영</b> 업무이다.</p>
+      <p>(약 4년, <em>2014.12. - 2018.12.</em>)</p>
+      <p>일진 전기의 '영업관리'시스템에서 부터 '일진그룹 전자입찰', '법인카드 정산'시스템 등 여러 업무시스템을 운영했다. IT시스템을 운영하면서 현업들로부터의 <b>요청사항</b> 및 <b>시스템 고도화</b>, 그리고 운영환경의 성능을 위한 <b>최적화</b> 등의 업무를 수행했다.</p>
+      <p>이 기간 동안 웹개발의 기본과 서버 환경(윈도우, 리눅스)에 대해 깊이 이해할 수 있었고, 가장 중요한 문제해결 능력이 대폭 상승되었다.</p><br/>
+      <p>다음으로 <b>솔루션 연구개발</b> 업무를 수행하였다.</p>
+      <br/><br/><hr><br/><br/>
+      <!-- ---------------- Divide Sections ---------------- -->
+      <h3>주요 사항</h3><br/>
+      <ul>
+          <li>Renders actual, "native" React DOM elements</li>
+          <li>Allows you to escape or skip HTML (try toggling the checkboxes above)</li>
+          <li>If you escape or skip the HTML, no dangerouslySetInnerHTML is used! Yay!</li>
+        </ul>
+        <br/><br/>
+        <hr>
+        <br/><br/>
+        <h3>Ordered list</h3>
+        <br/>
+        <ol>
+          <li>Analysis</li>
+          <li>Design</li>
+          <li>Implementation</li>
+        </ol>
+        <br/><br/>
+        <hr>
+        <br/><br/>
+        <h3>Blockquote</h3>
+        <br/><blockquote>Life is short, Smile while you still have teeth!&nbsp;</blockquote>
+        <br/><br/>
+        <hr>
+        <br/><br/>
+        <h3>Block Code</h3>\n\n<br/>\n\n<pre class="ql-syntax" spellcheck="false"><span class="hljs-keyword">import</span> <span class="hljs-title class_">React</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react\'</span>;\n<span class="hljs-keyword">import</span> <span class="hljs-title class_">ReactDOM</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react-dom\'</span>;\n<span class="hljs-keyword">import</span> <span class="hljs-title class_">ReactMarkdown</span> <span class="hljs-keyword">from</span> <span class="hljs-string">\'react-markdown\'</span>;\n<span class="hljs-keyword">import</span> rehypeHighlight <span class="hljs-keyword">from</span> <span class="hljs-string">\'rehype-highlight\'</span>;\n\n<span class="hljs-title class_">ReactDOM</span>.<span class="hljs-title function_">render</span>(\n  <span class="hljs-tag">&lt;<span class="hljs-name">ReactMarkdown</span> <span class="hljs-attr">rehypePlugins</span>=<span class="hljs-string">{[rehypeHighlight]}</span>&gt;</span>{\'# Your markdown here\'}<span class="hljs-tag">&lt;/<span class="hljs-name">ReactMarkdown</span>&gt;</span>,\n  <span class="hljs-variable language_">document</span>.<span class="hljs-title function_">querySelector</span>(<span class="hljs-string">\'#content\'</span>)\n);\n</pre>\n\n<br/>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<br/>\n<br/>\n<p>Why do we use it?</p>\n<br/>\n<br/>\n\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\n\n<br/>\n<br/>\n<p>\n<img src=https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_5.jpg />\n</p>\n<br/>\n<br/>\n\n<p>\n    It is important that you buy links because the links are what get you the results that you want. The popularity of the links that are listed in the MTA directory is in fact one of the most important factors in the performance of the search engine. Links are important and this is why you have to purchase a link in order to bid on something and the best part is that a link will only cost you $1, which is nothing compared to what you would pay if you decided to do it through any other company or website.\n</p>\n\n<br/>\n<br/>\n<p>\n    <img src=https://api-dev-minimal-v510.vercel.app/assets/images/cover/cover_14.jpg />\n</p>\n<br/>\n<br/>
+      `,
+
+    tags: ['web', 'dev', 'oss', 'iljin', 'Open Source'],
 
     totalComments: 0,
     totalFavorites: 0,
